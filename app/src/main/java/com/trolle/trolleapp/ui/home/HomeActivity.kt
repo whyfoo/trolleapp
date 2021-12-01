77c516844183a8b7e7647df5e0bd2875a8d084a6
Haidar Hanif <45656365+whyfoo@users.noreply.github.com>
2021-12-01 19:01:08 +0700

    Intent + Logo

--- app/src/main/java/com/trolle/trolleapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/trolle/trolleapp/ui/home/HomeActivity.kt
@@ -8,5 +8,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        supportActionBar?.apply {
+            setDisplayShowHomeEnabled(true)
+            setHomeButtonEnabled(true)
+            setDisplayUseLogoEnabled(true)
+            setLogo(R.drawable.menglogo)
+        }
     }
 }
--- app/src/main/java/com/trolle/trolleapp/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/trolle/trolleapp/ui/signin/SignInActivity.kt
@@ -1,11 +1,17 @@
 package com.trolle.trolleapp.ui.signin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.trolle.trolleapp.R
+import com.trolle.trolleapp.ui.home.HomeActivity
+import com.trolle.trolleapp.ui.main.MainActivity
+import com.trolle.trolleapp.ui.signup.SignUpActivity
 
 class SignInActivity : AppCompatActivity() {
 
@@ -14,6 +20,17 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val signinButton = findViewById<Button>(R.id.button_sign_in)
+        signinButton.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }
+
+        val signupButton = findViewById<TextView>(R.id.text_view_then_sign_up)
+        signupButton.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 
     override fun onBackPressed() {
