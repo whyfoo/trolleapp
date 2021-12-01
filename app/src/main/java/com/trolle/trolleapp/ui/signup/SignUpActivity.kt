e1c5e7875472abaa598b51c51bb30f2470d9a826
IvanWidjanarko <51114160+IvanWidjanarko@users.noreply.github.com>
2021-12-01 19:36:47 +0700

    Sign Up Page

--- app/src/main/java/com/trolle/trolleapp/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/trolle/trolleapp/ui/signup/SignUpActivity.kt
@@ -1,13 +1,31 @@
-
 package com.trolle.trolleapp.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import com.trolle.trolleapp.R
 
 class SignUpActivity : AppCompatActivity() {
+
+    private var backPressed: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
     }
+
+    override fun onBackPressed() {
+        if (backPressed) {
+            super.onBackPressed()
+            return
+        }
+        this.backPressed = true
+        Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
+        Handler(Looper.getMainLooper()).postDelayed({
+            this.backPressed = false
+        }, 2000)
+    }
+
 }
