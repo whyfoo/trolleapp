a5ca79187e5fe3e8740d8ba9cd06a6f82ffe3319
IvanWidjanarko <51114160+IvanWidjanarko@users.noreply.github.com>
2021-12-01 18:39:25 +0700

    Home Page

---
+++ app/src/main/java/com/trolle/trolleapp/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.trolle.trolleapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trolle.trolleapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/trolle/trolleapp/ui/signup/SignUpActivity.kt
@@ -0,0 +1,13 @@
+
+package com.trolle.trolleapp.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trolle.trolleapp.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
