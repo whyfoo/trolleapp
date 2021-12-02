1651429f96bd3e8bc1742f113c88f56d2e748eeb
IvanWidjanarko <51114160+IvanWidjanarko@users.noreply.github.com>
2021-12-02 16:56:39 +0700

    Home Failed Page

---
+++ app/src/main/java/com/trolle/trolleapp/ui/home/HomeFailedActivity.kt
@@ -0,0 +1,12 @@
+package com.trolle.trolleapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trolle.trolleapp.R
+
+class HomeFailedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_failed)
+    }
+}
