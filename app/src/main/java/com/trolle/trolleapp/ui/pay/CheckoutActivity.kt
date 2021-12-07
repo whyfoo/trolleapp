7d10233a2da5aa97ec9d130c8e74a7f8dc610513
IvanWidjanarko <51114160+IvanWidjanarko@users.noreply.github.com>
2021-12-07 17:02:38 +0700

    Checkout Page

---
+++ app/src/main/java/com/trolle/trolleapp/ui/pay/CheckoutActivity.kt
@@ -0,0 +1,12 @@
+package com.trolle.trolleapp.ui.pay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trolle.trolleapp.R
+
+class CheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
