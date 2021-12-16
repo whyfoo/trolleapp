9c3d39a5669e209dd175a46cbf439effcbd1caea
IvanWidjanarko <51114160+IvanWidjanarko@users.noreply.github.com>
2021-12-16 16:15:37 +0700

    Add Shopping List Page

---
+++ app/src/main/java/com/trolle/trolleapp/ui/pay/ShoppingListActivity.kt
@@ -0,0 +1,12 @@
+package com.trolle.trolleapp.ui.pay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trolle.trolleapp.R
+
+class ShoppingListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_list)
+    }
+}
