a9cf3764cd6fe8f3f9f2add1a1e4d3fa9b0aa7a5
IvanWidjanarko <51114160+IvanWidjanarko@users.noreply.github.com>
2021-12-16 15:35:56 +0700

    Help Page

---
+++ app/src/main/java/com/trolle/trolleapp/ui/side_menu/HelpActivity.kt
@@ -0,0 +1,12 @@
+package com.trolle.trolleapp.ui.side_menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trolle.trolleapp.R
+
+class HelpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help)
+    }
+}
