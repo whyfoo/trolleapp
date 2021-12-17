6b7f7f46a5568ee283f6f2cf2c25da9138c6d313
IvanWidjanarko <51114160+IvanWidjanarko@users.noreply.github.com>
2021-12-17 12:03:29 +0700

    Add Some Hyperlink

--- app/src/main/java/com/trolle/trolleapp/ui/side_menu/AboutActivity.kt
+++ app/src/main/java/com/trolle/trolleapp/ui/side_menu/AboutActivity.kt
@@ -3,10 +3,27 @@ package com.trolle.trolleapp.ui.side_menu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trolle.trolleapp.R
+import android.content.Intent
+import android.net.Uri
+import android.view.View
+
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+    }
+
+    fun openBrowser(view: View) {
+
+        //Get url from tag
+        val url = view.getTag() as String
+        val intent = Intent()
+        intent.action = Intent.ACTION_VIEW
+        intent.addCategory(Intent.CATEGORY_BROWSABLE)
+
+        //pass the url to intent data
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 }
