678e134665f1b7c95d98ba3a911d7f2bf7b1d3d4
Haidar Hanif <45656365+whyfoo@users.noreply.github.com>
2022-01-03 20:05:25 +0700

    Camera and Manual Input ID
    okay

--- app/src/main/java/com/trolle/trolleapp/HomeFragment.kt
+++ app/src/main/java/com/trolle/trolleapp/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.trolle.trolleapp
 
 import android.app.AlertDialog
+import android.content.*
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.EditText
 import androidx.navigation.Navigation
 import com.trolle.trolleapp.databinding.FragmentHomeBinding
 
@@ -32,16 +35,55 @@ class HomeFragment : Fragment() {
 
         binding.imageViewBarcode.setOnClickListener{
 
+            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePhotoIntent, 1)
+
+
+//            val alertDialog = AlertDialog.Builder(requireContext())
+//            alertDialog.apply {
+//                setTitle("Success!")
+//                setMessage("Your smartphone has successfully connected with Trolley ID = (ID). " +
+//                        "     Happy Shopping!")
+//                setPositiveButton("OK") { dialog, which ->
+//                    Navigation.findNavController(view).navigate(R.id.payFragment)
+//                }
+//                alertDialog.setNegativeButton("Cancel") { dialog, which ->
+//                    dialog.cancel()
+//                }
+//            }
+//            alertDialog.create().show()
+        }
+
+        binding.imageViewInput.setOnClickListener {
+
+            val input = EditText(context)
+            input.setHint("Enter ID")
+            input.inputType = InputType.TYPE_CLASS_NUMBER
+
+
             val alertDialog = AlertDialog.Builder(requireContext())
-            alertDialog.apply {
-                setTitle("Success!")
-                setMessage("Your smartphone has successfully connected with Trolley ID = (ID). " +
-                        "     Happy Shopping!")
-                setPositiveButton("OK") { _, _ ->
-                    //Toast.makeText(requireContext(),  "yes", Toast.LENGTH_SHORT).show()
-                    Navigation.findNavController(view).navigate(R.id.payFragment)
-                }.create().show()
+            alertDialog.setTitle("Input Trolley ID")
+            alertDialog.setView(input)
+            alertDialog.setPositiveButton("Connect") { dialog, which ->
+                var input = input.text.toString()
+
+                val alertDialogStatus = AlertDialog.Builder(requireContext())
+                alertDialogStatus.apply {
+                    setTitle("Success!")
+                    setMessage("Your smartphone has successfully connected with Trolley ID = " + input +
+                            "     Happy Shopping!")
+                    setPositiveButton("OK") { dialog, which ->
+                        Navigation.findNavController(view).navigate(R.id.payFragment)
+                    }
+                    alertDialogStatus.setNegativeButton("Cancel") { dialog, which ->
+                        dialog.cancel()
+                    }
+                }
+                alertDialogStatus.create().show()
             }
+            alertDialog.setNegativeButton("Cancel") { dialog, which ->
+                dialog.cancel()
+            }.create().show()
         }
     }
 }
